f7c1279aa0b1b015f03c4e6524fccdbeea116e60
jlg777 <proyectojlg777@gmail.com>
2023-10-07 13:06:07 -0300

    se agrega logica del login

--- js/login.js
+++ js/login.js
@@ -18,3 +18,42 @@ fetch(url, {
         alert("Registro exitoso!");
     });
 });
+
+var btn = document.getElementById("btnlogin");
+
+var loginModal = document.getElementById("loginForm1");
+
+btn.addEventListener("click",function() {
+    loginModal.style.display = "block";
+  });
+
+
+
+  var loginbtn = document.getElementById("loginForm");
+
+
+loginbtn.addEventListener("submit", function(event) {
+event.preventDefault(); // Evitar que se envíe el formulario automáticamente
+
+var loginUser = document.getElementById("loginEmail").value;
+var loginPass = document.getElementById("loginPassword").value;
+
+console.log("loginUser");
+console.log("loginPass")
+
+// Realizar la petición GET a la API para obtener los registros
+fetch("https://maquinas-agricolas-default-rtdb.europe-west1.firebasedatabase.app/users.json")
+.then(function(response) {
+  return response.json();
+})
+.then(function(data) {
+    // Buscar el registro que coincida con el DNI y la contraseña ingresados
+    for (var key in data) {
+        if (data.hasOwnProperty(key) && data[key].email_user === loginUser && data[key].pass_user === loginPass) {
+            alert("Credenciales correctas");
+          break;
+        }
+      } 
+})
+
+  });
