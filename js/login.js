e89ef3749c5e3247f75aab69a3c93f6051e11fab
jlg777 <proyectojlg777@gmail.com>
2023-10-18 19:09:03 -0300

    carga user

--- js/login.js
+++ js/login.js
@@ -1,17 +1,17 @@
 document.getElementById("registroForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Evitar que se envíe el formulario automáticamente
-var url = "https://maquinas-agricolas-default-rtdb.europe-west1.firebasedatabase.app/users1.json";
-var loginName = document.getElementById("loginNombre").value;
-var loginApp = document.getElementById("loginApellido").value;
-var user = document.getElementById("email").value;
-var pass = document.getElementById("password").value;
+var url = "https://maquinas-agricolas-default-rtdb.europe-west1.firebasedatabase.app/user1.json";
+var loginName = document.getElementById("exampleInputEmail1").value;
+var loginApp = document.getElementById("exampleInputEmail2").value;
+var user = document.getElementById("exampleInputEmail3").value;
+var pass = document.getElementById("exampleInputPassword1").value;
 var datos = {
   name_user: loginName,
   a_user: loginApp, 
   email_user: user,
   pass_user: pass
 };
-console.log(datos)
+console.log(datos);
 
 if (user==" "|| pass==" "){
   alert("Complete los datos!");
@@ -52,17 +52,18 @@ event.preventDefault(); // Evitar que se envíe el formulario automáticamente
 var loginUser = document.getElementById("loginEmail").value;
 var loginPass = document.getElementById("loginPassword").value;
 
-console.log("loginUser");
-console.log("loginPass")
+console.log(loginUser);
+console.log(loginPass)
 
 // Realizar la petición GET a la API para obtener los registros
-fetch("https://maquinas-agricolas-default-rtdb.europe-west1.firebasedatabase.app/users1.json")
+fetch("https://maquinas-agricolas-default-rtdb.europe-west1.firebasedatabase.app/user1.json")
 .then(function(response) {
   return response.json();
 })
 .then(function(data) {
     // Buscar el registro que coincida con el DNI y la contraseña ingresados
     for (var key in data) {
+      console.log(data[key].email_user)
         if (data.hasOwnProperty(key) && data[key].email_user === loginUser && data[key].pass_user === loginPass) {
            
           alert("Credenciales correctas!!");
