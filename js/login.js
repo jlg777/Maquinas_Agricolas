89ec1be0a3216b25f81dbcd468f286918c1c13ed
= <=>
2023-10-19 09:03:00 -0300

    cambiios

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 document.getElementById("registroForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Evitar que se envíe el formulario automáticamente
-var url = "https://maquinas-agricolas-default-rtdb.europe-west1.firebasedatabase.app/users1.json";
+var url = "https://maquinas-6f795-default-rtdb.firebaseio.com/user1.json";
 var loginName = document.getElementById("loginNombre").value;
 var loginApp = document.getElementById("loginApellido").value;
 var user = document.getElementById("email").value;
@@ -52,17 +52,18 @@ event.preventDefault(); // Evitar que se envíe el formulario automáticamente
 var loginUser = document.getElementById("loginEmail").value;
 var loginPass = document.getElementById("loginPassword").value;
 
-console.log("loginUser");
-console.log("loginPass")
+console.log(loginUser);
+console.log(loginPass)
 
 // Realizar la petición GET a la API para obtener los registros
-fetch("https://maquinas-agricolas-default-rtdb.europe-west1.firebasedatabase.app/users1.json")
+fetch("https://maquinas-6f795-default-rtdb.firebaseio.com/user1.json")
 .then(function(response) {
   return response.json();
 })
 .then(function(data) {
     // Buscar el registro que coincida con el DNI y la contraseña ingresados
     for (var key in data) {
+      console.log(data[key])
         if (data.hasOwnProperty(key) && data[key].email_user === loginUser && data[key].pass_user === loginPass) {
            
           alert("Credenciales correctas!!");
